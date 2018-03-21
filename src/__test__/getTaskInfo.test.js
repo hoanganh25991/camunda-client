febb931b8dec7cb9061993cfd603fba6ad769bf1
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-03-21 10:01:17 +0700

    Update test case name

--- src/__test__/getTaskInfo.test.js
+++ src/__test__/getTaskInfo.test.js
@@ -1,9 +1,22 @@
 import { getTaskInfo } from "../camundaIntegrate"
 
 const _ = console.log
-const restUrl = "http://localhost:8080/engine-rest"
-const taskId = "e75af187-271a-11e8-afab-e09d312afda0"
+const infoTestStatus = (pass, testCase) =>
+  pass ? _(`\x1b[42m[PASS]\x1b[0m ${testCase}`) : _(`\x1b[41m[FAIL]\x1b[0m ${testCase}`)
+
+// Run test
 ;(async () => {
-  const taskInfo = await getTaskInfo(restUrl, taskId)
-  _("[taskInfo]", taskInfo)
+  const TEST_CASE = "Get Task Info"
+  const restUrl = "http://localhost:8080/engine-rest"
+  const taskId = "e75af187-271a-11e8-afab-e09d312afda0"
+  let pass = true
+
+  try {
+    await getTaskInfo(restUrl, taskId)
+  } catch (err) {
+    _(`[${TEST_CASE}][ERR]`, err.message)
+    pass = false
+  } finally {
+    infoTestStatus(pass, TEST_CASE)
+  }
 })()
--- src/__test__/submit.test.js
+++ src/__test__/submit.test.js
@@ -1,9 +1,26 @@
 import { submit } from "../camundaIntegrate"
 
-const restUrl = "http://localhost:8080/engine-rest"
-const processId = "ExternalJsExample:1:fe933194-2756-11e8-a306-0a0027000004"
-const data = { jsonString: '{"q":12345}' } // Test go here
+const _ = console.log
+const infoTestStatus = (pass, testCase) =>
+  pass ? _(`\x1b[42m[PASS]\x1b[0m ${testCase}`) : _(`\x1b[41m[FAIL]\x1b[0m ${testCase}`)
+
+// Run test
 ;(async () => {
-  const resData = await submit(restUrl, processId, data)
-  console.log("[resData]", resData)
+  const TEST_CASE = "Start Process to create task"
+  const restUrl = "http://localhost:8080/engine-rest"
+  const processId = "ExternalJsExample:1:fe933194-2756-11e8-a306-0a0027000004"
+  const data = { jsonString: '{"q":12345}' }
+  let pass = true
+
+  try {
+    const resData = await submit(restUrl, processId, data)
+    _("[resData]", resData)
+
+    pass = !!resData
+  } catch (err) {
+    _(`[${TEST_CASE}][ERR]`, err.message)
+    pass = false
+  } finally {
+    infoTestStatus(pass, TEST_CASE)
+  }
 })()
