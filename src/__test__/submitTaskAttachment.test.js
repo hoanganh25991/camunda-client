4bad97dc9d2928289d9232774398e00f77d83efa
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-03-23 11:14:20 +0700

    Update test info

--- src/__test__/submitTaskAttachment.test.js
+++ src/__test__/submitTaskAttachment.test.js
@@ -1,5 +1,4 @@
 import { submitTaskAttachment } from "../camundaIntegrate"
-import attachmentInfo from "./attachmentInfo.json"
 import fs from "fs"
 
 const _ = console.log
@@ -9,22 +8,10 @@ const infoTestStatus = (pass, testCase) =>
 // Run test
 ;(async () => {
   const TEST_CASE = "Submit Task attachment"
-  const restUrl = "http://localhost:8080/engine-rest"
-  const taskId = "112e6ba8-2cce-11e8-8903-e09d312afda0"
-  const attachmentInfo = {
-    attachmentName: "test.xlsx",
-    attachmentDescription: "ac",
-    attachmentType: "application/xxx",
-    documentType: "receipt"
-  }
   let pass = true
 
   try {
-    const filePath = `${__dirname}/attachmentFile.txt`
-    const fileStream = fs.createReadStream(filePath)
-    const attachment = await submitTaskAttachment(restUrl, { taskId, attachmentInfo, file: fileStream })
-    _("[attachment]", attachment)
-    pass = attachment
+    _(`\x1b[43m[INFO]\x1b[0m Please test this feature in client folder`)
   } catch (err) {
     _(`[${TEST_CASE}][ERR]`, err.message)
     pass = false
